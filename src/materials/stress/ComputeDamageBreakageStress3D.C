768939168c245d57b03cbf2c0706ce997dba1525
chunhuizhao478 <chunhui3@illinois.edu>
2024-11-24 21:28:52 -0600

    close tangent modulus calculation

--- src/materials/stress/ComputeDamageBreakageStress3D.C
+++ src/materials/stress/ComputeDamageBreakageStress3D.C
@@ -240,13 +240,13 @@ ComputeDamageBreakageStress3D::computeQpStress()
   _elastic_strain[_qp] = eps_e;
 
   // Compute jacobian //_Jacobian_mult[_qp]
-  computeQpTangentModulus(I1, 
-                          I2, 
-                          xi, 
-                          B_out,
-                          shear_modulus_out, 
-                          gamma_damaged_out, 
-                          a0, a1, a2, a3, eps_e);
+  // computeQpTangentModulus(I1, 
+  //                         I2, 
+  //                         xi, 
+  //                         B_out,
+  //                         shear_modulus_out, 
+  //                         gamma_damaged_out, 
+  //                         a0, a1, a2, a3, eps_e);
 
   //Compute equivalent strain rate
   RankTwoTensor epsilon_rate = (eps_p - _eps_p_old[_qp])/_dt;
@@ -340,109 +340,109 @@ ComputeDamageBreakageStress3D::alphacr_root2(Real xi, Real gamma_damaged_r) {
     return 2 * _shear_modulus_o / (gamma_damaged_r * (xi - 2 * _xi_0));
 }
 
-void
-ComputeDamageBreakageStress3D::computeQpTangentModulus(Real I1, 
-                                                       Real I2, 
-                                                       Real xi, 
-                                                       Real B,
-                                                       Real shear_modulus_out,
-                                                       Real gamma_damaged_out,
-                                                       Real a0,
-                                                       Real a1,
-                                                       Real a2,
-                                                       Real a3,
-                                                       RankTwoTensor Ee)
-{
+// void
+// ComputeDamageBreakageStress3D::computeQpTangentModulus(Real I1, 
+//                                                        Real I2, 
+//                                                        Real xi, 
+//                                                        Real B,
+//                                                        Real shear_modulus_out,
+//                                                        Real gamma_damaged_out,
+//                                                        Real a0,
+//                                                        Real a1,
+//                                                        Real a2,
+//                                                        Real a3,
+//                                                        RankTwoTensor Ee)
+// {
 
-  // //define functions for derivatives dstress_dstrain
-  RankFourTensor tangent;
+//   // //define functions for derivatives dstress_dstrain
+//   RankFourTensor tangent;
 
-  //delta function
-  auto delta = [](int i, int j) -> Real {
-    return (i == j) ? 1.0 : 0.0;
-  };
+//   //delta function
+//   auto delta = [](int i, int j) -> Real {
+//     return (i == j) ? 1.0 : 0.0;
+//   };
 
-  //dI1_dE_{kl}
-  auto dI1dE = [&](int k, int l) -> Real {
-    return delta(k,l);
-  };
+//   //dI1_dE_{kl}
+//   auto dI1dE = [&](int k, int l) -> Real {
+//     return delta(k,l);
+//   };
 
-  //dI2_dE_{kl}
-  auto dI2dE = [&](int k, int l) -> Real {
-    return 2 * Ee(k,l);
-  };
+//   //dI2_dE_{kl}
+//   auto dI2dE = [&](int k, int l) -> Real {
+//     return 2 * Ee(k,l);
+//   };
 
-  //dxi_dE_{kl}
-  auto dxidE = [&](int k, int l) -> Real {
+//   //dxi_dE_{kl}
+//   auto dxidE = [&](int k, int l) -> Real {
     
-    // Epsilon to avoid division by zero
-    const Real epsilon = 1e-12;
-    // Adjust I2 if necessary
-    Real adjusted_I2 = I2;
-    if (I2 <= epsilon) {
-      //mooseWarning("I2 is zero or too small (I2 = ", I2, "), adjusting to epsilon.");
-      adjusted_I2 = epsilon;
-    }
+//     // Epsilon to avoid division by zero
+//     const Real epsilon = 1e-12;
+//     // Adjust I2 if necessary
+//     Real adjusted_I2 = I2;
+//     if (I2 <= epsilon) {
+//       //mooseWarning("I2 is zero or too small (I2 = ", I2, "), adjusting to epsilon.");
+//       adjusted_I2 = epsilon;
+//     }
 
-    Real dxidE = 0.5 * pow(adjusted_I2,-1.5) * dI2dE(k,l) * I1;
-    //mooseInfo("I1 = ", I1, ", I2 = ", I2);
-    if (std::isnan(dxidE)){mooseError("dxidE");}
-    return delta(k,l) * pow(adjusted_I2,-0.5) - 0.5 * pow(adjusted_I2,-1.5) * dI2dE(k,l) * I1;
-  };
+//     Real dxidE = 0.5 * pow(adjusted_I2,-1.5) * dI2dE(k,l) * I1;
+//     //mooseInfo("I1 = ", I1, ", I2 = ", I2);
+//     if (std::isnan(dxidE)){mooseError("dxidE");}
+//     return delta(k,l) * pow(adjusted_I2,-0.5) - 0.5 * pow(adjusted_I2,-1.5) * dI2dE(k,l) * I1;
+//   };
 
-  //dE_{ij}_dE_{kl}
-  auto dEdE = [&](int i, int j, int k, int l) -> Real {
-    return delta(i,k) * delta(j,l);
-    //return 0.5 * ( delta(i,k) * delta(j,l) + delta(i,l) * delta(j,k) ); //its symmetric form
-  };
+//   //dE_{ij}_dE_{kl}
+//   auto dEdE = [&](int i, int j, int k, int l) -> Real {
+//     return delta(i,k) * delta(j,l);
+//     //return 0.5 * ( delta(i,k) * delta(j,l) + delta(i,l) * delta(j,k) ); //its symmetric form
+//   };
 
-  //dxi^{-1}_dE_{kl}
-  auto dxim1dE = [&](int k, int l) -> Real {
-    return -1.0 * pow(xi,-2.0) * dxidE(k,l);
-  };
+//   //dxi^{-1}_dE_{kl}
+//   auto dxim1dE = [&](int k, int l) -> Real {
+//     return -1.0 * pow(xi,-2.0) * dxidE(k,l);
+//   };
 
-  //dxi^3_dE_{kl}
-  auto dxi3dE = [&](int k, int l) -> Real {
-    return 3 * pow(xi,2) * dxidE(k,l);
-  };
+//   //dxi^3_dE_{kl}
+//   auto dxi3dE = [&](int k, int l) -> Real {
+//     return 3 * pow(xi,2) * dxidE(k,l);
+//   };
 
-  //dSe_{ij}_dE_{kl}
-  auto dSedE = [&](int i, int j, int k, int l) -> Real {
-    Real dSedE_components = (- gamma_damaged_out * dxim1dE(k,l) ) * I1 * delta(i,j);
-    dSedE_components += ( _lambda_o - gamma_damaged_out / xi ) * dI1dE(k,l) * delta(i,j);
-    dSedE_components += (- gamma_damaged_out * dxidE(k,l) ) * Ee(i,j);
-    dSedE_components += ( 2 * shear_modulus_out - gamma_damaged_out * xi ) * dEdE(i,j,k,l);
-    if (std::isnan(dSedE_components)){mooseError("dSedE_components");}
-    return dSedE_components;
-  };
+//   //dSe_{ij}_dE_{kl}
+//   auto dSedE = [&](int i, int j, int k, int l) -> Real {
+//     Real dSedE_components = (- gamma_damaged_out * dxim1dE(k,l) ) * I1 * delta(i,j);
+//     dSedE_components += ( _lambda_o - gamma_damaged_out / xi ) * dI1dE(k,l) * delta(i,j);
+//     dSedE_components += (- gamma_damaged_out * dxidE(k,l) ) * Ee(i,j);
+//     dSedE_components += ( 2 * shear_modulus_out - gamma_damaged_out * xi ) * dEdE(i,j,k,l);
+//     if (std::isnan(dSedE_components)){mooseError("dSedE_components");}
+//     return dSedE_components;
+//   };
 
-  //dSb_{ij}_dE_{kl}
-  auto dSbdE = [&](int i, int j, int k, int l) -> Real {
-    Real dSbdE_components = ( a1 * dxim1dE(k,l) + 3 * a3 * dxidE(k,l) ) * I1 * delta(i,j);
-    dSbdE_components += ( 2 * a2 + a1 / xi + 3 * a3 * xi ) * dI1dE(k,l) * delta(i,j);
-    dSbdE_components += ( a1 * dxidE(k,l) - a3 * dxi3dE(k,l) ) * Ee(i,j);
-    dSbdE_components += ( 2 * a0 + a1 * xi - a3 * pow(xi,3) ) * dEdE(i,j,k,l);
-    return dSbdE_components;
-  };
+//   //dSb_{ij}_dE_{kl}
+//   auto dSbdE = [&](int i, int j, int k, int l) -> Real {
+//     Real dSbdE_components = ( a1 * dxim1dE(k,l) + 3 * a3 * dxidE(k,l) ) * I1 * delta(i,j);
+//     dSbdE_components += ( 2 * a2 + a1 / xi + 3 * a3 * xi ) * dI1dE(k,l) * delta(i,j);
+//     dSbdE_components += ( a1 * dxidE(k,l) - a3 * dxi3dE(k,l) ) * Ee(i,j);
+//     dSbdE_components += ( 2 * a0 + a1 * xi - a3 * pow(xi,3) ) * dEdE(i,j,k,l);
+//     return dSbdE_components;
+//   };
 
-  //dS_{ij}_dE_{kl}
-  auto dSdE = [&](int i, int j, int k, int l) -> Real {
-    return (1 - B) * dSedE(i,j,k,l) + B * dSbdE(i,j,k,l);
-  };
+//   //dS_{ij}_dE_{kl}
+//   auto dSdE = [&](int i, int j, int k, int l) -> Real {
+//     return (1 - B) * dSedE(i,j,k,l) + B * dSbdE(i,j,k,l);
+//   };
 
-  // Compute tangent modulus C
-  for (unsigned int i = 0; i < _dim; i++){
-    for (unsigned int j = 0; j < _dim; j++){
-      for (unsigned int k = 0; k < _dim; k++){
-        for (unsigned int l = 0; l < _dim; l++){
-          if (std::isnan(dSdE(i,j,k,l))){mooseError("encounter nan error: dSdE(i,j,k,l)");}
-          tangent(i,j,k,l) += dSdE(i,j,k,l);
-        }
-      }
-    }
-  }
+//   // Compute tangent modulus C
+//   for (unsigned int i = 0; i < _dim; i++){
+//     for (unsigned int j = 0; j < _dim; j++){
+//       for (unsigned int k = 0; k < _dim; k++){
+//         for (unsigned int l = 0; l < _dim; l++){
+//           if (std::isnan(dSdE(i,j,k,l))){mooseError("encounter nan error: dSdE(i,j,k,l)");}
+//           tangent(i,j,k,l) += dSdE(i,j,k,l);
+//         }
+//       }
+//     }
+//   }
 
-  //update jacobian
-  _Jacobian_mult[_qp] = tangent;
+//   //update jacobian
+//   _Jacobian_mult[_qp] = tangent;
 
-}
+// }
