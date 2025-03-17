8c5480d3a59a17e5a4ab53ea9b240b6702ceed90
chunhuizhao478 <chunhui3@illinois.edu>
2025-03-17 17:09:20 -0500

    update log: critical fix on PerturbationRadial magnitude, rerun the code

--- src/materials/damage/PerturbationRadial.C
+++ src/materials/damage/PerturbationRadial.C
@@ -93,7 +93,7 @@ PerturbationRadial::computeQpProperties()
   Real scaled_gaussian_value = 0.0;
   if (_t <= _duration)
   {
-    scaled_gaussian_value = gaussian_value * (_t / _duration);
+    scaled_gaussian_value = gaussian_value / (_duration / _dt);
   }
   else
   {
