f7238682724a39a0122101177ab297bde48d7cb5
chunhuizhao478 <chunhui3@illinois.edu>
2025-10-22 21:48:20 -0500

    postprocess: extract elastic solution alpha0/0.3, dsigma10, dc0d8

--- examples/3dslipweakening/parametric_study/postprocess/post_onfault_mpi.py
+++ examples/3dslipweakening/parametric_study/postprocess/post_onfault_mpi.py
@@ -611,6 +611,24 @@ CASES = [
         "output_dir": COMMON_PATH
         + "../postprocess/alpha0d5_dsigma16_Dc1d2_db_on_fault/",
     },
+    {
+        "name": "dynamic_solve_alpha0_dsigma10_Dc0d8_elastic_csv_main_fault",
+        "data_dir": COMMON_PATH,
+        "file_prefix": "dynamic_solve_alpha0_dsigma10_Dc0d8_elastic_csv_main_fault_",
+        "index": {"start": 2, "end": 1800, "step": 2, "pad": 4},
+        "dt": 0.005,
+        "output_dir": COMMON_PATH
+        + "../postprocess/alpha0_dsigma10_Dc0d8_elastic_on_fault/",
+    },
+    {
+        "name": "dynamic_solve_alpha0d3_dsigma10_Dc0d8_elastic_csv_main_fault",
+        "data_dir": COMMON_PATH,
+        "file_prefix": "dynamic_solve_alpha0d3_dsigma10_Dc0d8_elastic_csv_main_fault_",
+        "index": {"start": 2, "end": 1800, "step": 2, "pad": 4},
+        "dt": 0.005,
+        "output_dir": COMMON_PATH
+        + "../postprocess/alpha0d3_dsigma10_Dc0d8_elastic_on_fault/",
+    },
 ]
 
 # Optional global output root. If set (non-None), outputs will be written to
